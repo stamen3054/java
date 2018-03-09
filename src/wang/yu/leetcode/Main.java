61a1ab8646c8aaddc102e4e3435df27df67033df
Wang <yw050121@cerner.com>
2018-03-09 14:16:29 -0600

    根据问题类型添加了多个package

---
+++ src/wang/yu/leetcode/Main.java
@@ -0,0 +1,14 @@
+
+package wang.yu.leetcode;
+
+/**
+ * @author Yu Wang
+ */
+public class Main
+{
+   public static void main(String[] args)
+   {
+      LeetCode leetCode = new LeetCode();
+      leetCode.selfDividingNumbers(101, 708);
+   }
+}
---
+++ src/wang/yu/leetcode/TreeNode.java
@@ -0,0 +1,18 @@
+
+package wang.yu.leetcode;
+
+public class TreeNode
+{
+   int val;
+   TreeNode left;
+   TreeNode right;
+   public TreeNode(int x)
+   {
+      // TODO Auto-generated constructor stub
+      val = x;
+   }
+   
+   public void test(){
+      
+   }
+}
